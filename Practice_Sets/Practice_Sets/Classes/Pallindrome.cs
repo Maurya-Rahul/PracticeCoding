1a70225f5a358220d936a93ae4dc1b0cf1b5894e
Rahul <rahulkumarmaurya3710@gmail.com>
2025-05-23 16:34:10 +0530

    Added solution files

---
+++ Practice_Sets/Practice_Sets/Classes/Pallindrome.cs
@@ -0,0 +1,27 @@
+﻿namespace Practice_Sets.Classes
+{
+    internal static class Pallindrome
+    {
+        public static void isPallindrome(string input)
+        {
+            input = input.ToLower();
+            Console.WriteLine($"input : {input}");
+
+            // Suppose string is pallindrome initially
+            bool flag = true;
+
+            for (int i = 0, j = input.Length - 1; i <= input.Length / 2; i++, j--)
+            {
+                if (input[i] != input[j])
+                {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag)
+                Console.WriteLine($"{input} is pallindrome");
+            else
+                Console.WriteLine($"{input} is not pallindrome");
+        }
+    }
+}
---
+++ Practice_Sets/Practice_Sets/Classes/ReverseString.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace Practice_Sets.Classes
+{
+    internal static class ReverseString
+    {
+        public static string Reverse(string input)
+        {
+            Console.WriteLine($" input string : {input}");
+            char[] charArray = input.ToCharArray();
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = charArray.Length - 1; i >= 0; i--)
+            {
+                sb.Append(charArray[i]);
+            }
+
+            Console.WriteLine($"output = {sb.ToString()}");
+            return sb.ToString();
+        }
+    }
+}
