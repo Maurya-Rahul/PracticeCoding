adcc2f7befc181b5bf99eed2d78dc725665a9db5
Rahul <rahulkumarmaurya3710@gmail.com>
2025-05-23 16:09:29 +0530

    Added initial changes for coding

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+// Reverse String
+using Practice_Sets.Classes;
+
+string input = "Lilliput";
+//Console.WriteLine($"Hello, calling {nameof(ReverseString.Reverse)} with input string : {input}");
+//ReverseString.Reverse(input);
+
+Pallindrome.isPallindrome(input);
+
+Console.ReadLine();
+
